7061398d11f271d326197e80aeffc4a5352186eb
bvtk21 <127777059+bvtk21@users.noreply.github.com>
2023-03-19 08:15:24 +0500

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,5 +1,5 @@
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
 
 engines = openai.Engine.list()
 print(engines)
